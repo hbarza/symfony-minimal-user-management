1aa5f888b97286f2ae5e512252b5a69cb034a674
Omid <hbarza@gmail.com>
2018-08-31 18:11:28 +0430

    create database, run migratoin and create tables

--- src/Migrations/Version20180831131553.php
+++ src/Migrations/Version20180831131553.php
@@ -15,7 +15,7 @@ final class Version20180831131553 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE user_entity (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(64) NOT NULL, firstname VARCHAR(32) NOT NULL, lastname VARCHAR(32) DEFAULT NULL, create_at DATETIME NOT NULL, update_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_entity (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(64) NOT NULL UNIQUE, firstname VARCHAR(32) NOT NULL, lastname VARCHAR(32) DEFAULT NULL, create_at DATETIME NOT NULL DEFAULT NOW(), update_at DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW(), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
--- src/Migrations/Version20180831131958.php
+++ src/Migrations/Version20180831131958.php
@@ -15,11 +15,10 @@ final class Version20180831131958 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE user_entity (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(64) NOT NULL, firstname VARCHAR(32) NOT NULL, lastname VARCHAR(32) DEFAULT NULL, create_at DATETIME NOT NULL, update_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user_group (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(64) DEFAULT NULL, create_at DATETIME NOT NULL, update_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user_group_user_entity (user_group_id INT NOT NULL, user_entity_id INT NOT NULL, INDEX IDX_B6DDFBBF1ED93D47 (user_group_id), INDEX IDX_B6DDFBBF81C5F0B9 (user_entity_id), PRIMARY KEY(user_group_id, user_entity_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE user_group_user_entity ADD CONSTRAINT FK_B6DDFBBF1ED93D47 FOREIGN KEY (user_group_id) REFERENCES user_group (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE user_group_user_entity ADD CONSTRAINT FK_B6DDFBBF81C5F0B9 FOREIGN KEY (user_entity_id) REFERENCES user_entity (id) ON DELETE CASCADE');
+        $this->addSql('CREATE TABLE user_group (id INT AUTO_INCREMENT NOT NULL, `name` VARCHAR(64) NOT NULL, create_at DATETIME NOT NULL DEFAULT NOW(), update_at DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW(), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_group_user_entity (user_group_id INT NOT NULL, user_entity_id INT NOT NULL, INDEX IDX_USER_GROUP_ID (user_group_id), INDEX IDX_USER_ENTITY_ID (user_entity_id), PRIMARY KEY(user_group_id, user_entity_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user_group_user_entity ADD CONSTRAINT FK_USER_GROUP_ID FOREIGN KEY (user_group_id) REFERENCES user_group (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE user_group_user_entity ADD CONSTRAINT FK_USER_ENTITY_ID FOREIGN KEY (user_entity_id) REFERENCES user_entity (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema) : void
@@ -27,9 +26,8 @@ final class Version20180831131958 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user_group_user_entity DROP FOREIGN KEY FK_B6DDFBBF81C5F0B9');
-        $this->addSql('ALTER TABLE user_group_user_entity DROP FOREIGN KEY FK_B6DDFBBF1ED93D47');
-        $this->addSql('DROP TABLE user_entity');
+        $this->addSql('ALTER TABLE user_group_user_entity DROP FOREIGN KEY FK_USER_ENTITY_ID');
+        $this->addSql('ALTER TABLE user_group_user_entity DROP FOREIGN KEY FK_USER_GROUP_ID');
         $this->addSql('DROP TABLE user_group');
         $this->addSql('DROP TABLE user_group_user_entity');
     }
