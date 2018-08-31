66f7d370aad4f0259db964e6e04baa9d15704bb8
Omid <hbarza@gmail.com>
2018-08-31 17:12:32 +0430

    entities unit test ready

---
+++ tests/Entity/U0010UserGroupUnitTest.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use PHPUnit\Framework\TestCase;
+use App\Entity\UserGroup;
+
+class U0010UserGroupUnitTest extends KernelTestCase
+{
+
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+
+    private $doctrine;
+
+    /**
+     * List of properteis to test their setter and getter
+     * 
+     */
+    private $_properties = [
+        'name'     => 'Test Group',
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $kernel = self::bootKernel();
+
+        $this->doctrine      = $kernel->getContainer()->get('doctrine');
+        $this->entityManager = $this->doctrine->getManager();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->entityManager->close();
+        $this->entityManager = null; // avoid memory leaks
+
+        $this->doctrine = null; // avoid memory leaks
+    }
+
+    /**
+     * Test UserGroup properties by setting a value and get it and make suer 
+     * those are same
+     */
+    public function testPropertiesSetterGetter()
+    {
+        $userGroup = new UserGroup;
+        foreach ($this->_properties as $property => $value) {
+            $setter = "set$property";
+            $getter = "get$property";
+            $userGroup->$setter($value);
+            $this->assertSame($userGroup->$getter(), $value);
+        }
+    }
+
+    /**
+     * Test repository collection
+     */
+    public function testGetRepositoryCollection()
+    {
+        $count  = 5;
+        for ($i = 1; $i <= $count; $i++) {
+            $random = $this->_getRandomNumber();
+            $name  = "Test $random$i";
+            $this->_addGroupEntity($name);
+        }
+
+        $groupsRepo = $this->entityManager
+            ->getRepository(UserGroup::class)
+            ->findAll();
+
+        $this->assertCount($count, $groupsRepo);
+    }
+    
+    /**
+     * Test add new group entity and find it
+     */
+    public function testGroupEntityAddAndFind()
+    {
+        $random     = $this->_getRandomNumber();
+        $groupId    = $this->_addGroupEntity("Test $random")->getId();
+        $this->assertGreaterThanOrEqual(1, $groupId);
+
+        $groupEntity = $this->entityManager
+            ->getRepository(UserGroup::class)
+            ->find($groupId);
+
+        $this->assertSame($groupId, $groupEntity->getId());
+    }
+
+    /**
+     * Remove group entity row from db
+     */
+    public function testGroupEntityAddAndRemove()
+    {
+        $random     = $this->_getRandomNumber();
+        $groupEntity = $this->_addGroupEntity("Test $random");
+        $this->assertGreaterThanOrEqual(1, $groupEntity->getId());
+
+        $this->entityManager->remove($groupEntity);
+        $this->entityManager->persist($groupEntity);
+        $this->entityManager->flush();
+        
+        $this->assertInstanceOf(UserGroup::class, $groupEntity);
+    }
+
+    /**
+     * internal method to add group for tests
+     * 
+     * @param   string  $name
+     */
+    protected function _addGroupEntity(string $name)
+    {
+        $groupEntity = new UserGroup;
+        $groupEntity->setName("$name Group");
+        $groupEntity->setCreateAt(new \DateTime);
+        $groupEntity->setUpdateAt(new \DateTime);
+
+        $this->entityManager->persist($groupEntity);
+        $this->entityManager->flush();
+
+        return $groupEntity;
+    }
+
+    protected function _getRandomNumber()
+    {
+        return random_int(1, 9999999999);
+    }
+}
---
+++ tests/Entity/U0020UserEntityUnitTest.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\Entity\UserEntity;
+use App\Entity\UserGroup;
+
+class U0020UserEntityUnitTest extends KernelTestCase
+{
+
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+
+    private $doctrine;
+
+    /**
+     * List of properteis to test their setter and getter
+     * 
+     * @var array
+     */
+    private $_properties = [
+        'email'     => 'test@tester.com',
+        'firstname' => 'Omid',
+        'lastname'  => 'Barza'
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $kernel = self::bootKernel();
+
+        $this->doctrine      = $kernel->getContainer()->get('doctrine');
+        $this->entityManager = $this->doctrine->getManager();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->entityManager->close();
+        $this->entityManager = null; // avoid memory leaks
+
+        $this->doctrine = null; // avoid memory leaks
+    }
+
+    /**
+     * Test UserEntity properties by setting a value and get it and make suer 
+     * those are same
+     */
+    public function testPropertiesSetterGetter()
+    {
+        $userEntity = new UserEntity;
+        foreach ($this->_properties as $property => $value) {
+            $setter = "set$property";
+            $getter = "get$property";
+            $userEntity->$setter($value);
+            $this->assertSame($userEntity->$getter(), $value);
+        }
+    }
+
+    /**
+     * Test repository collection
+     */
+    public function testGetRepositoryCollection()
+    {
+        $count  = 10;
+        for ($i = 1; $i <= $count; $i++) {
+            $random = $this->_getRandomNumber();
+            $email  = "repoTest$random$i";
+            $this->_addUserEntity($email);
+        }
+
+        $usersRepo = $this->entityManager
+            ->getRepository(UserEntity::class)
+            ->findAll();
+
+        $this->assertCount($count, $usersRepo);
+    }
+    
+    // /**
+    //  * @depends UserEntityKernelTest::testRepository
+    //  */
+    /**
+     * Test add new user entity and find it
+     */
+    public function testUserEntityAddAndFind()
+    {
+        $random     = $this->_getRandomNumber();
+        $userId     = $this->_addUserEntity("test$random")->getId();
+        $this->assertGreaterThanOrEqual(1, $userId);
+
+        $userEntity = $this->entityManager
+            ->getRepository(UserEntity::class)
+            ->find($userId);
+
+        $this->assertSame($userId, $userEntity->getId());
+    }
+
+    /**
+     * Adding groups to user entity test and update those to new groups
+     */
+    public function testAssignGroupToUser()
+    {
+        $userGroups = $this->entityManager
+            ->getRepository(UserGroup::class)
+            ->findAll();
+
+        $random     = $this->_getRandomNumber();
+        $userEntity = $this->_addUserEntity("testassign$random");
+        $result     = null;
+        $i = 0;
+        foreach ($userGroups as $id => $userGrop) {
+            if ($i >= 3) {
+                break;
+            }
+            $result = $userEntity->addUserGroup($userGrop);
+            $this->assertInstanceOf(UserEntity::class, $result);
+            unset($userGroups[$id]);
+        }
+        $updateGroupsIds = [];
+        $i = 0;
+        foreach ($userGroups as $userGroup) {
+            if ($i >= 3) {
+                break;
+            }
+            $updateGroupsIds[] = $userGroup->getId();
+        }
+        $result = $userEntity->saveUserGroups($updateGroupsIds, $this->doctrine);
+        $this->assertInstanceOf(UserEntity::class, $result);
+    }
+
+    /**
+     * Remove user entity row from db
+     */
+    public function testUserEntityAddAndRemove()
+    {
+        $random     = $this->_getRandomNumber();
+        $userEntity = $this->_addUserEntity("test$random");
+        $this->assertGreaterThanOrEqual(1, $userEntity->getId());
+
+        $this->entityManager->remove($userEntity);
+        $this->entityManager->persist($userEntity);
+        $this->entityManager->flush();
+            
+        $this->assertInstanceOf(UserEntity::class, $userEntity);
+    }
+
+    /**
+     * internal method to add user for tests
+     * 
+     * @param   string  $email
+     */
+    protected function _addUserEntity(string $email)
+    {
+        $userEntity = new UserEntity();
+        $userEntity->setEmail("$email@tester.com");
+        $userEntity->setFirstname('Omid');
+        $userEntity->setLastname('Barza');
+        $userEntity->setCreateAt(new \DateTime);
+        $userEntity->setUpdateAt(new \DateTime);
+
+        $this->entityManager->persist($userEntity);
+        $this->entityManager->flush();
+
+        return $userEntity;
+    }
+
+    protected function _getRandomNumber()
+    {
+        return random_int(1, 9999999999);
+    }
+}
